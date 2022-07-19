a3c7158b9f6fbbfa001325571e9fae44cfeed886
xXstrawcakeXx <107396089+xXstrawcakeXx@users.noreply.github.com>
2022-07-18 18:12:23 -0700

    Followed demo up to W5D4 @11:01:33 -1:08:14

---
+++ src/main/java/com/revature/model/User.java
@@ -0,0 +1,75 @@
+package com.revature.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name = "users")
+@Data //generate getters/setters, toString, hashCode, and equals methods automatically
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	//data validation: length of fields must be >1
+	//comes from Hibernate, I guess?
+	@Length(min=2)
+	private String firstName;
+	
+	@Length(min=2)
+	private String lastName;
+	
+	//Data validation: min length of 5 and alphanumeric
+	@NotBlank
+	@Length(min=5)
+	@Pattern(regexp = "[a-zA-Z][a-zA-Z0-9]*")
+	private String username;
+	
+	@NotBlank
+	private String password;
+	
+	//Data validation: well-formed email requirement (ex. must have @ sign)
+	@Email // from javax.validation.constrains. Every email must contain an @
+	private String email;
+
+	public User(@Length(min = 2) String firstName, @Length(min=2) String lastName,
+			@NotBlank @Length(min = 5) @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9]*") String userName,
+			@NotBlank String password, @Email String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = userName;
+		this.password = password;
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+
+	
+}
---
+++ src/main/java/com/revature/service/UserService.java
@@ -0,0 +1,70 @@
+package com.revature.service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.revature.data.UserRepository;
+import com.revature.exception.UserNotFoundException;
+import com.revature.model.User;
+
+public class UserService {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	//methods that call upon the Repo
+	public Set<User> findAll(){
+		
+		//return from user repo the findAll method but stream it to a set
+		return userRepo.findAll().stream().collect(Collectors.toSet());
+	}
+	
+	//Find by username
+	public User getByUsername(String username) {
+		
+		return userRepo.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException("No User found with username: " + username));
+	}
+	
+	public User getById(int id) {
+		if(id<=0) {
+			log.warn("Id must be greater than zero: {}", id);
+		}
+		return userRepo.getById(id);
+	}
+	
+	public User add(User u) {
+		
+		User returnedUser = userRepo.save(u);
+		//getId() below is from lombok
+		if(returnedUser.getId() > 0) {
+			log.info("Successfully returned user with id {}", returnedUser.getId());
+		}else {
+			log.warn("Could not add user");
+		}
+		
+		return returnedUser;
+	}
+	
+	
+	public User update(User u) {
+		return userRepo.save(u);
+	}
+	
+	public boolean delete(int id) {
+		userRepo.deleteById(id);
+		//validates if deletion went through
+		return !(userRepo.existsById(id));
+	}
+	
+	
+	
+	
+	
+}
