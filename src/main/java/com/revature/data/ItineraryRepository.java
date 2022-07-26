25b8ae295e56cf9cff5af6e052a7b5fca27e4f0b
xXstrawcakeXx <107396089+xXstrawcakeXx@users.noreply.github.com>
2022-07-25 23:43:25 -0700

    CART WORRRRRKSSSSSS. Also tested all controller methods on Postman.: they all work except for findByTag in ItineraryController but that's because I have not tested it yet
    MVP Requirements (Backend tasks):
    
    - As a User, I should be able to log into the application. (DONE)
    - As a User, I should be able to register a new account. (DONE)
    - As a User, I should be able to create and maintain a profile page. (N/A to backend)
    As a User, I should be able to see a list of available products for me to add to my cart (DONE)
    As a User, I should be able to add items to my cart
     that I will later purchase or remove from my cart. (DONE)
    As a User, I should be able to checkout with the items in my cart, purchasing them and removing them from the inventory (N/A to backend?)

--- src/main/java/com/revature/data/ItineraryRepository.java
+++ src/main/java/com/revature/data/ItineraryRepository.java
@@ -3,11 +3,16 @@ package com.revature.data;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.Itinerary;
+import com.revature.model.User;
 
 @Repository
 public interface ItineraryRepository extends JpaRepository<Itinerary, Integer>{
@@ -21,6 +26,16 @@ public interface ItineraryRepository extends JpaRepository<Itinerary, Integer>{
 	
 	@Query(value = "SELECT * FROM itineraries WHERE itinerary_id IN (SELECT itin_id FROM itin_tags WHERE tag_id = ?1)", nativeQuery = true)
 	public List<Itinerary> findByTag(int tag_id);
+	
+	@Modifying
+	@Transactional
+	@Query(value="UPDATE itineraries SET slots=slots-1 WHERE \"itinerary_id\" =:itiner_id AND slots>0", nativeQuery = true)
+	public int addToCart(@Param("itiner_id") int itiner_id);
+	
+	@Modifying
+	@Transactional
+	@Query(value="UPDATE itineraries SET slots=slots+1 WHERE itinerary_id=:it_id AND slots>0", nativeQuery = true)
+	public int removeFromCart(@Param("it_id") int itiner_id);
 	
 	
 }
