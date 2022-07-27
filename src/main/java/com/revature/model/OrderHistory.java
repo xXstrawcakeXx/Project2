c30f97b11587f804457c5e61a5b7f70e136a935f
William Moore <wilchasemoore@gmail.com>
2022-07-27 13:52:55 -0500

    Merge pull request #45 from xXstrawcakeXx/Neffy
    Added Cart but mapping doesn't work

--- src/main/java/com/revature/model/OrderHistory.java
+++ src/main/java/com/revature/model/OrderHistory.java
@@ -1,5 +1,38 @@
 package com.revature.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orderhistory")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderHistory {
 
+	@Id
+	@Column(name = "order_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private double price;
+	
+	private String date;
+	
+	@OneToOne
+	@JoinColumn(name = "acc_owner", referencedColumnName = "user_id")
+	private User user;
+	
+	
+	
 }
